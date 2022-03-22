a89eff889a74596ae9f4709d058a970833ae5ac8
Joshua <54748294+Joshuajrodrigues@users.noreply.github.com>
2022-03-22 23:36:31 +0530

    base game logic completed

--- RockPaperScissorCSharp/Program.cs
+++ RockPaperScissorCSharp/Program.cs
@@ -9,14 +9,51 @@ namespace RockPaperScissorCSharp
             Console.WriteLine("******************");
             Console.WriteLine("****Rock Paper Scissors*****");
             Console.WriteLine("******************");
+            Console.WriteLine("Type 'r' for rock, 'p' for paper and 's' for scissor. Q to exit");
+            string userInput = Console.ReadLine();
+
             string[] choiseArr = { "Rock", "Paper", "Scissor" };
             Random rand = new Random();
             int index = rand.Next(choiseArr.Length);
-            Console.WriteLine("Type 'r' for rock, 'p' for paper and 's' for scissor");
-            string userInput = Console.ReadLine();
+            string computerChoise = choiseArr[index];
 
-            Console.WriteLine("Your choice is " + userInput);
-            Console.WriteLine("Computer choise is " + choiseArr[index]);
+
+            switch (userInput)
+            {
+                case "s":
+                    Console.WriteLine("Your choice is Scissor ");
+                    Console.WriteLine("Computer choise is " + computerChoise);
+                    break;
+                case "p":
+                    Console.WriteLine("Your choice is Paper ");
+                    Console.WriteLine("Computer choise is " + computerChoise);
+                    break;
+                case "r":
+                    Console.WriteLine("Your choice is Rock ");
+                    Console.WriteLine("Computer choise is " + computerChoise);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+
+            if(userInput == computerChoise.ToLower().Substring(0, 1))
+            {
+                Console.WriteLine("Its a tie.");
+            }
+            else if((userInput == "r" && computerChoise == "Paper") || (userInput == "p" && computerChoise == "Scissor") || (userInput == "s" && computerChoise == "Rock"))
+            {
+                Console.WriteLine("Computer wins!");
+            }
+            else if(( computerChoise == "r" && userInput == "Paper") || (computerChoise == "p" && userInput  == "Scissor") || (computerChoise == "s" && userInput  == "Rock"))
+            {
+                Console.WriteLine("User wins!");
+            }
+            else
+            {
+                Console.WriteLine("Please enter correct key.");
+            }
+
 
             Console.ReadLine();
         }
