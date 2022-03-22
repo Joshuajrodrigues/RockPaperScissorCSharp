9cde45ae0c517a46ab2137fb1f96ce59572e54d2
Joshua <54748294+Joshuajrodrigues@users.noreply.github.com>
2022-03-22 22:56:33 +0530

    Add project files.

---
+++ RockPaperScissorCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RockPaperScissorCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******************");
+            Console.WriteLine("****Rock Paper Scissors*****");
+            Console.WriteLine("******************");
+            string[] choiseArr = { "Rock", "Paper", "Scissor" };
+            Random rand = new Random();
+            int index = rand.Next(choiseArr.Length);
+            Console.WriteLine("Type 'r' for rock, 'p' for paper and 's' for scissor");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine("Your choice is " + userInput);
+            Console.WriteLine("Computer choise is " + choiseArr[index]);
+
+            Console.ReadLine();
+        }
+    }
+}
